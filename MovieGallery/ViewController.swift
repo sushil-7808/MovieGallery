ffe78b68bb6bbf1074acc8d47cfc272717ba9feb
Sushil Karki <sushil.karki7808@gmail.com>
2020-02-04 21:59:45 -0800

    Initial Commit

---
+++ MovieGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieGallery
+//
+//  Created by Sushil Karki on 2/4/20.
+//  Copyright © 2020 Sushil Karki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieGalleryTests/MovieGalleryTests.swift
@@ -0,0 +1,34 @@
+//
+//  MovieGalleryTests.swift
+//  MovieGalleryTests
+//
+//  Created by Sushil Karki on 2/4/20.
+//  Copyright © 2020 Sushil Karki. All rights reserved.
+//
+
+import XCTest
+@testable import MovieGallery
+
+class MovieGalleryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
